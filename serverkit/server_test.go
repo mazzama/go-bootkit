7a14febf70e59add86832f466e48420932b7ad1f
mazzama <mazzamabduljabbar@gmail.com>
2026-02-04 22:49:41 +0700

    test: add WebServer creation tests

---
+++ serverkit/server_test.go
@@ -0,0 +1,52 @@
+package serverkit
+
+import (
+	"log/slog"
+	"testing"
+	"time"
+
+	"github.com/mazzama/go-bootkit/core/healthkit"
+)
+
+// Task 14: Test ServerKit - WebServer Creation
+
+func TestNewWebServer(t *testing.T) {
+	logger := slog.Default()
+	health := healthkit.NewAggregator(5 * time.Second)
+
+	server := NewWebServer("test-server", ":8080",
+		WithWebServerLogger(logger),
+		WithHealthAggregator(health),
+	)
+
+	if server == nil {
+		t.Fatal("NewWebServer() = nil, want non-nil")
+	}
+	if server.name != "test-server" {
+		t.Errorf("name = %v, want 'test-server'", server.name)
+	}
+	if server.addr != ":8080" {
+		t.Errorf("addr = %v, want ':8080'", server.addr)
+	}
+	if server.logger != logger {
+		t.Error("logger not set")
+	}
+	if server.health != health {
+		t.Error("health aggregator not set")
+	}
+	if server.router == nil {
+		t.Error("router not initialized")
+	}
+}
+
+func TestNewWebServer_Defaults(t *testing.T) {
+	health := healthkit.NewAggregator(0)
+	server := NewWebServer("test", ":8080", WithHealthAggregator(health))
+
+	if server.health != health {
+		t.Error("health aggregator should be set when provided")
+	}
+	if server.logger != nil {
+		t.Error("logger should be nil by default")
+	}
+}
